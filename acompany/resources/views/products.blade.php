7c8873b6ea0c5c38f49ca6b7cf80bcdf7dbbe6f1
Elyzia Janara <103510445+Elyz-ra@users.noreply.github.com>
2023-11-29 20:45:29 +0700

    Products

--- acompany/resources/views/products.blade.php
+++ acompany/resources/views/products.blade.php
@@ -2,124 +2,148 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Acompany</title>
+  <title>Acompany</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
-    <!-- Font -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Maven+Pro:wght@400;500;600;900&display=swap" rel="stylesheet">
+  <!-- Font -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Maven+Pro:wght@400;500;600;900&display=swap" rel="stylesheet">
 
-    <!-- CSS -->
-    <!-- <link rel="stylesheet" href="{{ asset('css/customer.css') }}">
-    @vite('resources/css/css/customer.css') -->
+  <!-- CSS -->
+  <link rel="stylesheet" href="{{ asset('css/products.css') }}">
+  @vite('resources/css/css/products.css')
 </head>
 
 <body style="font-family: 'Maven Pro';">
-    <!-- SideBar -->
-    @extends('component/sideBar')
-      <h1 class="heading-title">Semua Produk</h1>
-
-      <div class="flex flex-col ml-64 p-4">
-        <div class="grid gap-y-10 gap-x-6 md:grid-cols-2 xl:grid-cols-4">
-            <div class="card relative flex flex-col border-none rounded-xl text-gray-700" style="background-color: #E5ECF6;">
-                <div class="px-4 p-3 text-left">
-                    <p class="block antialiased leading-normal font-semibold text-blue-gray-600">Pertamax Turbo</p>
-                </div>
-                <div class="card-img-container mt-4 mb-5">
-                  <img src="{{ asset('images/pertamaxTurbo.png') }}" alt="Product 1" class="card-img" style="width: 300px;">
-                  <p class="card-description mt-4">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim minima, minus voluptas dolor nisi ullam veritatis similique praesentium incidunt ex odit animi voluptatibus voluptatem possimus consectetur neque ducimus maiores id?
-                  </p>
-                </div>
-                <div class="px-10 p-1 text-left flex items-center justify-between">
-                <div>
-                  <p class="card-price">Price: Rp20.000/liter</p>
-                  <p class="card-stock">Stock: 1000 (liter)</p>
-                </div>
-                </div>
-            </div>
-
-            <div class="card relative flex flex-col border-none rounded-xl text-gray-700" style="background-color: #E5ECF6;">
-                <div class="px-4 p-3 text-left">
-                    <p class="block antialiased leading-normal font-semibold text-blue-gray-600">Pertalite</p>
-                </div>
-                <div class="card-img-container mt-4 mb-5">
-                  <img src="{{ asset('images/pertalite.png') }}" alt="Product 2" class="card-img" style="width: 300px;">
-                  <p class="card-description mt-4">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim minima, minus voluptas dolor nisi ullam veritatis similique praesentium incidunt ex odit animi voluptatibus voluptatem possimus consectetur neque ducimus maiores id?
-                  </p>
-                </div>
-                <div class="px-10 p-1 text-left flex items-center justify-between">
-                <div>
-                  <p class="card-price">Price: Rp20.000/liter</p>
-                  <p class="card-stock">Stock: 1000 (liter)</p>
-                </div>
-                </div>
-            </div>
-
-            <div class="card relative flex flex-col border-none rounded-xl text-gray-700" style="background-color: #E5ECF6;">
-                <div class="px-4 p-3 text-left">
-                    <p class="block antialiased leading-normal font-semibold text-blue-gray-600">Pertamina Dex</p>
-                </div>
-                <div class="card-img-container mt-4 mb-5">
-                  <img src="{{ asset('images/pertaminaDex.png') }}" alt="Product 3" class="card-img" style="width: 300px;">
-                  <p class="card-description mt-4">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim minima, minus voluptas dolor nisi ullam veritatis similique praesentium incidunt ex odit animi voluptatibus voluptatem possimus consectetur neque ducimus maiores id?
-                  </p>
-                </div>
-                <div class="px-10 p-1 text-left flex items-center justify-between">
-                <div>
-                  <p class="card-price">Price: Rp20.000/liter</p>
-                  <p class="card-stock">Stock: 1000 (liter)</p>
-                </div>
-                </div>
-            </div>
-
-            <div class="card relative flex flex-col border-none rounded-xl text-gray-700" style="background-color: #E5ECF6;">
-                <div class="px-4 p-3 text-left">
-                    <p class="block antialiased leading-normal font-semibold text-blue-gray-600">Dexlite</p>
-                </div>
-                <div class="card-img-container mt-4 mb-5">
-                  <img src="{{ asset('images/dexLite.png') }}" alt="Product 4" class="card-img" style="width: 300px;">
-                  <p class="card-description mt-4">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim minima, minus voluptas dolor nisi ullam veritatis similique praesentium incidunt ex odit animi voluptatibus voluptatem possimus consectetur neque ducimus maiores id?
-                  </p>
-                </div>
-                <div class="px-10 p-1 text-left flex items-center justify-between">
-                <div>
-                  <p class="card-price">Price: Rp20.000/liter</p>
-                  <p class="card-stock">Stock: 1000 (liter)</p>
-                </div>
-                </div>
-            </div>
+  <!-- SideBar -->
+  @extends('component/sideBar')
+  <div class="flex flex-col ml-64 p-4">
+    <div class="overflow-x-auto sm:mx-0.5 lg:mx-0.5">
+      <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+        <div class="overflow-hidden">
+          <table class="min-w-full">
+            <thead class="bg-white border-b">
+              <tr>
+                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                  No
+                </th>
+                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                  Product ID
+                </th>
+                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                  Product Name
+                </th>
+                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                  Stock
+                </th>
+                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                  Unit Price
+                </th>
+                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                  Last Update
+                </th>
+                </th>
+                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                  Sales ID
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr class="bg-gray-100 border-b">
+                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">1</td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  01
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  Pertamax Turbo
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  178L
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  Rp15.900
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  11-29-2023
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  101
+                </td>
+              </tr>
+              <tr class="bg-white border-b">
+                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">2</td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  02
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  Pertalite
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  287L
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  Rp10.000
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  11-29-2023
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  102
+                </td>
+              </tr>
+              <tr class="bg-white border-b">
+                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">3</td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  03
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  Pertamina Dex
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  639L
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  Rp16.900
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  11-29-2023
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  103
+                </td>
+              </tr>
+              <tr class="bg-white border-b">
+                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">4</td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  04
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  Dex Lite
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  402L
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  Rp17.200
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  11-29-2023
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  104
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
+      </div>
     </div>
-
-      <!-- <section>
-      <div class="card-container">
-        <a href="#" class="card">
-          <div class="card-img-container">
-            <img src="{{ asset('images/logoPertamina.png') }}" alt="Product 1" class="card-img">
-          </div>
-
-          <div class="card-details">
-            <h1 class="card-title">Pertamina</h1>
-            <div>
-              <p class="card-price">Rp20.000</p>
-              <p class="card-stock">1000</p>
-            </div>
-            <p class="card-description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim minima, minus voluptas dolor nisi ullam veritatis similique praesentium incidunt ex odit animi voluptatibus voluptatem possimus consectetur neque ducimus maiores id?
-            </p>
-          </div>
-        </a>
-      </div> 
-      </section>      -->
+  </div>
 </body>
-</html>
 
+</html>
